078165043c9255bc0d28912dac86fa7eedc6010b
MINYOUNGKIM <801sanae@gmail.com>
2022-01-22 22:38:39 +0900

    first commit

---
+++ src/test/java/com/hanati/onepass/OnepassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanati.onepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
